a472c0f21ead37b7bf6ffaef0b283435d8341c0e
stayforever03 <83788358+stayforever03@users.noreply.github.com>
2023-02-04 12:51:40 -0500

    play button

--- run_app.py
+++ run_app.py
@@ -1,23 +1,36 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 class RunApp():
 
     def change_button(self):
         if self.is_playing:
             self.is_playing = False
-            self.play_button["text"] = "Pause"
+            self.play_button["image"] = self.playBtn
 
         else:
             self.is_playing = True
-            self.play_button["text"] = "Start"
-
+            self.play_button["image"] = self.pauseBtn
+            self.input_prompt = self.text.get("1.0","end-1c") #do this only at the beginning
+            print(self.input_prompt)
 
     def __init__(self):
         self.root = tk.Tk()
         self.is_playing = False
+        self.input_prompt = ""
 
+        self.text = tk.Text(self.root, height = 5, width = 40)
+        self.text.pack(padx=20, pady=20)
 
-        self.play_button = tk.Button(self.root, text="Start Discussion", font=("Comic Sans", 18), command = self.change_button)
+        play = Image.open("Media/icons8-play-64.png")
+        play = play.resize((64, 64))
+        self.playBtn = ImageTk.PhotoImage(play) 
+
+        pause = Image.open("Media/icons8-pause-64.png")
+        pause = pause.resize((64, 64))
+        self.pauseBtn = ImageTk.PhotoImage(pause) 
+
+        self.play_button = tk.Button(self.root, image=self.playBtn, font=("Comic Sans", 18), command = self.change_button, height = 64, width = 64, borderwidth=0)
         self.play_button.pack(padx=20, pady=20)
 
         self.root.geometry("500x500")
--- speech_to_text.py
+++ speech_to_text.py
@@ -1,5 +1,4 @@
 import json
-
 import base64
 import asyncio
 import pyaudio
